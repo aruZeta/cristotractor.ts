abcc97d2438953275d29f21ab9ca06c5abd5f9a0
aruZeta <aru_hackZ.official@zohomail.eu>
2022-10-29 13:12:58 +0200

    [add] Embed interface

---
+++ src/interfaces/embed.ts
@@ -0,0 +1,50 @@
+import { time } from "discord.js";
+
+import Cristotractor from "../client";
+
+export interface IEmbed {
+  color: number,
+  timestamp: string,
+  author: IEmbedAuthor,
+  title?: string,
+  description?: string,
+  url?: string,
+  thumbnail?: IEmbedThumbnail,
+  footer?: IEmbedFooter,
+  fields?: IEmbedField[],
+};
+
+export interface IEmbedFooter {
+  text: string,
+  icon_url: string,
+};
+
+export interface IEmbedAuthor {
+  name: string,
+  url: string,
+  icon_url: string,
+};
+
+export interface IEmbedThumbnail {
+  url: string,
+  height?: number,
+  width?: number,
+};
+
+export interface IEmbedField {
+  name: string,
+  value: string,
+  inline?: boolean,
+};
+
+export const genDefaultEmbed = (): IEmbed => {
+  return {
+    color: 0x1abc96,
+    timestamp: new Date().toISOString(),
+    author: {
+      name: "Cristotractor",
+      url: "https://github.com/aruZeta/cristotractor.ts",
+      icon_url: Cristotractor.config.bot.userPfp,
+    },
+  }
+};
