cda4f81851555a266cd1116c3cf587b768460f0e
aruZeta <aru_hackZ.official@zohomail.eu>
2022-10-29 20:05:04 +0200

    [remove] Author nickname

--- src/models/author.ts
+++ src/models/author.ts
@@ -5,14 +5,12 @@ import { VehicleModel } from "./vehicle";
 export interface IAuthor {
   _id: Types.ObjectId,
   name: string,
-  nickname: string,
   vehicles: Types.ObjectId[],
 };
 
 export const authorSchema: Schema<IAuthor> = new Schema({
   _id: { type: Schema.Types.ObjectId },
   name: { type: String, required: true },
-  nickname: { type: String, required: true },
   vehicles: { type: [Schema.Types.ObjectId], ref: VehicleModel },
 });
 
