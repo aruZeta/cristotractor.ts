05da7679ce9272d977e0133ff0c3da6c10dd0b70
aruZeta <aru_hackZ.official@zohomail.eu>
2022-10-29 13:13:26 +0200

    [refactor] Using embeds in ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,18 @@
 import { CommandInteraction } from "discord.js";
 
 import { ICommand, ECommandType } from "../interfaces/command";
+import { genDefaultEmbed, IEmbed } from "../interfaces/embed";
 
 export const command: ICommand = {
   name: "ping",
   description: "Responde con un pong.",
   type: ECommandType.chatInput,
   run: async (interaction: CommandInteraction): Promise<any> => {
-    await interaction.reply("Pong!")
+    const msgEmbed: IEmbed = genDefaultEmbed()
+    msgEmbed.title = "Pong!";
+
+    await interaction.reply({
+      embeds: [msgEmbed],
+    });
   },
 };
