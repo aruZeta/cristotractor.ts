0c3349f16f8a14ba5d788dde5280b2c05048728e
aruZeta <aru_hackZ.official@zohomail.eu>
2022-10-31 11:45:04 +0100

    [add] Unique author names

--- src/models/author.ts
+++ src/models/author.ts
@@ -12,7 +12,7 @@ export interface IAuthor {
 };
 
 export const authorSchema: Schema<IAuthor> = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   vehicles: { type: [Schema.Types.ObjectId], ref: VehicleModel, required: true },
   phrases: { type: [Schema.Types.ObjectId], ref: PhraseModel, required: true },
   bias: { type: Number, required: true },
