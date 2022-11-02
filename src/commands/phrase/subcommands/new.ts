ef2cc68ee40e07a7b157d9b8a8f2eee4ba52492f
aruZeta <aru_hackZ.official@zohomail.eu>
2022-11-02 10:12:56 +0100

    [add] Require author to add phrases

--- src/commands/phrase/subcommands/new.ts
+++ src/commands/phrase/subcommands/new.ts
@@ -33,7 +33,7 @@ export const subcommand: ICommandOption = {
     name: "autor",
     description: "Autor de la frase",
     type: ECommandOptionType.string,
-    required: false,
+    required: true,
     choices: Cristotractor.mongoCache.authors.map(
       (_: any, key: string): ICommandOptionChoice => {
         return { name: key, value: key };
@@ -54,11 +54,9 @@ export const run = async (
   const phrase: string = capitalize(
     <string>interaction.options.getString("frase")
   );
-  const author: string | null = interaction.options.getString("autor");
-  const authorID: Types.ObjectId | null =
-    author
-      ? <Types.ObjectId>Cristotractor.mongoCache.authors.get(author)
-      : null;
+  const author: string = <string>interaction.options.getString("autor");
+  const authorID: Types.ObjectId =
+    <Types.ObjectId>Cristotractor.mongoCache.authors.get(author)
 
   PhraseModel.create({
     phrase: phrase,
@@ -77,7 +75,7 @@ export const run = async (
         bias: 0
       });
     }
-    if (authorID) await AuthorModel.updateOne(
+    await AuthorModel.updateOne(
       { _id: authorID },
       { $push: { phrases: phraseDoc._id } }
     );
